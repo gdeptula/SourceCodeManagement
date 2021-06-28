f8410dea8eae80e5da06084ad6016ba0da0c8cf9
Gerred Deptula <gerred.deptula@outlook.com>
2021-06-27 22:55:22 -0400

    First comment.

---
+++ SourceCodeManagement/SourceCodeManagementSource.cpp
@@ -0,0 +1,16 @@
+/*Gerred Deptula
+  CIS 1202 5T1
+  Soruce Code Management
+  June 27, 2021*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello, World!";
+	
+	cout << "\n\n";
+	system("pause");
+	return 0;
+}
